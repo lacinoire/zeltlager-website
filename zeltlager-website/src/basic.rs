d3a80095557c0a59809af51fda7bcfbf2082ff73
Flakebi <flakebi@t-online.de>
2018-03-30 18:57:54 +0200

    Add some parts for templating

--- zeltlager-website/src/basic.rs
+++ zeltlager-website/src/basic.rs
@@ -1,11 +1,17 @@
-#[derive(Deserialize, Debug)]
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
+
+use {Result, toml};
+
+#[derive(Clone, Deserialize, Debug)]
 pub struct SiteDescription {
     pub name: String,
     pub title: String,
     pub description: String,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Clone, Deserialize, Debug)]
 pub struct SiteDescriptions {
     pub sites: Vec<SiteDescription>,
 }
@@ -14,7 +20,33 @@ pub struct SiteDescriptions {
 #[TemplatePath = "templates/basic.tt"]
 #[derive(Debug)]
 pub struct Basic<'a> {
-    pub all_sites: &'a [SiteDescription],
+    pub all_sites: &'a SiteDescriptions,
     pub description: &'a SiteDescription,
     pub content: String,
+}
+
+impl SiteDescriptions {
+    pub fn parse() -> Result<Self> {
+        let mut content = String::new();
+        File::open(Path::new("dynamic").join("basic.toml"))?.read_to_string(&mut content)?;
+        Ok(toml::from_str(&content)?)
+    }
+
+    pub fn get_site(&self, name: &str) -> Result<Basic> {
+        // Check if this site exists
+        for site in &self.sites {
+            if site.name == name {
+                // Read file
+                let mut content = String::new();
+                File::open(Path::new("dynamic").join(&site.name))?.read_to_string(&mut content)?;
+
+                return Ok(Basic {
+                    all_sites: self,
+                    description: site,
+                    content,
+                });
+            }
+        }
+        bail!("Cannot find site {}", name)
+    }
 }
--- zeltlager-website/src/main.rs
+++ zeltlager-website/src/main.rs
@@ -1,9 +1,12 @@
 extern crate actix_web;
+#[macro_use]
+extern crate failure;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 #[macro_use]
 extern crate t4rust_derive;
+extern crate toml;
 
 use std::fs::File;
 use std::io::Read;
@@ -12,11 +15,23 @@ use actix_web::*;
 
 mod basic;
 
-fn index(_req: HttpRequest) -> &'static str {
-    ""
+type Result<T> = std::result::Result<T, failure::Error>;
+
+struct AppState {
+    basics: basic::SiteDescriptions,
 }
 
-fn not_found(_: HttpRequest) -> Result<HttpResponse> {
+fn index(req: HttpRequest<AppState>) -> Result<HttpResponse> {
+    let name: String = req.match_info().query("name")?;
+    let site = req.state().basics.get_site(&name)?;
+    let content = format!("{}", site);
+
+    Ok(httpcodes::HttpNotFound.build()
+       .content_type("text/html; charset=utf-8")
+       .body(content)?)
+}
+
+fn not_found(_: HttpRequest<AppState>) -> Result<HttpResponse> {
     let mut content = String::new();
     File::open("static/404.html")?.read_to_string(&mut content)?;
     Ok(httpcodes::HttpNotFound.build()
@@ -25,13 +40,15 @@ fn not_found(_: HttpRequest) -> Result<HttpResponse> {
 }
 
 fn main() {
-    HttpServer::new(
-        || Application::new()
+    HttpServer::new(|| {
+        let basics = basic::SiteDescriptions::parse().expect("Failed to parse basic.toml");
+
+        Application::with_state(AppState { basics })
             .middleware(middleware::Logger::default())
             .handler("/static", fs::StaticFiles::new("static", false)
                 .default_handler(not_found))
             .resource("/{name}", |r| r.f(index))
-            .default_resource(|r| r.f(not_found)))
-        .bind("127.0.0.1:8080").unwrap()
+            .default_resource(|r| r.f(not_found))
+    }).bind("127.0.0.1:8080").unwrap()
         .run();
 }
