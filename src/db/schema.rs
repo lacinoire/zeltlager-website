3d7c5a753b1087dc6f1d466295175a951ed3426e
Carolin Brandt <carolin.brandt@tum.de>
2018-04-29 17:25:55 +0200

    update schema

--- src/db/schema.rs
+++ src/db/schema.rs
@@ -1,45 +1,48 @@
 table! {
-	betreuer (id) {
-		id -> Int4,
-		vorname -> Text,
-		nachname -> Text,
-		geburtsdatum -> Date,
-		geschlecht -> Text,
-		juleica_nummer -> Text,
-		mail -> Text,
-		handynummer -> Text,
-		strasse -> Text,
-		hausnummer -> Text,
-		ort -> Text,
-		plz -> Text,
-		besonderheiten -> Text,
-		agb -> Bool,
-		selbsterklaerung -> Bool,
-		fuehrungszeugnis_auststellung -> Date,
-		fuehrungszeugnis_eingesehen -> Nullable<Date>,
-	}
+    betreuer (id) {
+        id -> Int4,
+        vorname -> Text,
+        nachname -> Text,
+        geburtsdatum -> Date,
+        geschlecht -> Text,
+        juleica_nummer -> Text,
+        mail -> Text,
+        handynummer -> Text,
+        strasse -> Text,
+        hausnummer -> Text,
+        ort -> Text,
+        plz -> Text,
+        besonderheiten -> Text,
+        agb -> Bool,
+        selbsterklaerung -> Bool,
+        fuehrungszeugnis_auststellung -> Date,
+        fuehrungszeugnis_eingesehen -> Nullable<Date>,
+    }
 }
 
 table! {
-	teilnehmer (id) {
-		id -> Int4,
-		vorname -> Text,
-		nachname -> Text,
-		geburtsdatum -> Date,
-		geschlecht -> Text,
-		schwimmer -> Bool,
-		vegetarier -> Bool,
-		tetanus_impfung -> Bool,
-		eltern_name -> Text,
-		eltern_mail -> Text,
-		eltern_handynummer -> Text,
-		strasse -> Text,
-		hausnummer -> Text,
-		ort -> Text,
-		plz -> Text,
-		besonderheiten -> Text,
-		agb -> Bool,
-	}
+    teilnehmer (id) {
+        id -> Int4,
+        vorname -> Text,
+        nachname -> Text,
+        geburtsdatum -> Date,
+        geschlecht -> Text,
+        schwimmer -> Bool,
+        vegetarier -> Bool,
+        tetanus_impfung -> Bool,
+        eltern_name -> Text,
+        eltern_mail -> Text,
+        eltern_handynummer -> Text,
+        strasse -> Text,
+        hausnummer -> Text,
+        ort -> Text,
+        plz -> Text,
+        besonderheiten -> Text,
+        agb -> Bool,
+    }
 }
 
-allow_tables_to_appear_in_same_query!(betreuer, teilnehmer,);
+allow_tables_to_appear_in_same_query!(
+    betreuer,
+    teilnehmer,
+);
