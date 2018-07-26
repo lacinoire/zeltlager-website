323d7694eb3e30481019b509c18d394c23b83daf
Flakebi <flakebi@t-online.de>
2018-07-26 10:48:08 +0200

    Add cookie consent message

--- static/js/zeltlager.js
+++ static/js/zeltlager.js
@@ -2,3 +2,24 @@ $(document).on('click', '[data-toggle="lightbox"]', function(event) {
 	event.preventDefault();
 	$(this).ekkoLightbox({alwaysShowClose: true});
 });
+
+window.addEventListener("load", function(){
+window.cookieconsent.initialise({
+	"palette": {
+		"popup": {
+			"background": "#aaa"
+		},
+		"button": {
+			"background": "#f1d600"
+		}
+	},
+	"content": {
+		"message": `<div style="font-weight: bold; text-align: center;">Diese Webseite verwendet Cookies</div>
+
+			Wir verwenden Cookies ausschließlich, um die Sicherheit des Logins zu gewährleisten und Ihre Einstellungen zu speichern.
+			Sie geben Ihre Einwilligung für die Nutzung von Cookies, wenn Sie unsere Webseite weiterhin nuzten.`,
+		"dismiss": "Ich stimme zu",
+		"link": "Ausführliche Informationen finden Sie in unserer Datenschutzerklärung",
+		"href": "/datenschutz"
+	}
+})});
