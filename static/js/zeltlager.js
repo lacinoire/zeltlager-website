e52a1f27eb9f3f7eb92a9de79c7aec1e7fe7afc9
Flakebi <flakebi@t-online.de>
2019-03-10 11:59:13 +0100

    Add show password checkbox to login site

--- static/js/zeltlager.js
+++ static/js/zeltlager.js
@@ -3,23 +3,34 @@ $(document).on('click', '[data-toggle="lightbox"]', function(event) {
 	$(this).ekkoLightbox({alwaysShowClose: true});
 });
 
-window.addEventListener("load", function(){
-window.cookieconsent.initialise({
-	"palette": {
-		"popup": {
-			"background": "#aaa"
-		},
-		"button": {
-			"background": "#f1d600"
-		}
-	},
-	"content": {
-		"message": `<div style="font-weight: bold; text-align: center;">Diese Webseite verwendet Cookies</div>
+window.addEventListener("load", function() {
+	$(document).ready(function() {
+		$("#showPassword").change(function() {
+			if (this.checked) {
+				$('#password').attr('type', 'text');
+			} else {
+				$('#password').attr('type', 'password');
+			}
+		});
+	});
 
-			Wir verwenden Cookies ausschließlich, um die Sicherheit des Logins zu gewährleisten und Ihre Einstellungen zu speichern.
-			Sie geben Ihre Einwilligung für die Nutzung von Cookies, wenn Sie unsere Webseite weiterhin nuzten.`,
-		"dismiss": "Ich stimme zu",
-		"link": "Ausführliche Informationen finden Sie in unserer Datenschutzerklärung",
-		"href": "/datenschutz"
-	}
-})});
+	window.cookieconsent.initialise({
+		"palette": {
+			"popup": {
+				"background": "#aaa"
+			},
+			"button": {
+				"background": "#f1d600"
+			}
+		},
+		"content": {
+			"message": `<div style="font-weight: bold; text-align: center;">Diese Webseite verwendet Cookies</div>
+
+				Wir verwenden Cookies ausschließlich, um die Sicherheit des Logins zu gewährleisten und Ihre Einstellungen zu speichern.
+				Sie geben Ihre Einwilligung für die Nutzung von Cookies, wenn Sie unsere Webseite weiterhin nuzten.`,
+			"dismiss": "Ich stimme zu",
+			"link": "Ausführliche Informationen finden Sie in unserer Datenschutzerklärung",
+			"href": "/datenschutz"
+		}
+	})
+});
