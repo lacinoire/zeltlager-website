9d847267037ad5b24125099b86b55f48990f1339
Flakebi <flakebi@t-online.de>
2022-10-08 21:18:04 +0200

    Add glightbox

---
+++ frontend/src/lib/GLightbox.svelte
@@ -0,0 +1,8 @@
+<script type="ts">
+    // glightbox from https://github.com/biati-digital/glightbox under MIT
+    import glightbox from "./glightbox/glightbox";
+</script>
+
+<style>
+    @import "glightbox/glightbox.css";
+</style>
