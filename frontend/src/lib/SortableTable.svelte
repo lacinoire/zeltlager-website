bbb11ee535a64544399b77e939c636b3ab758278
Flakebi <flakebi@t-online.de>
2023-01-27 23:57:32 +0100

    Forgot file

---
+++ frontend/src/lib/SortableTable.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import SortIcon from "./SortIcon.svelte";
+    import type { Column } from "./utils";
+
+    export let columns: Column[];
+    export let sortBy = "";
+</script>
+
+<table class="table is-striped is-hoverable">
+    <thead class="is-sticky">
+        <tr>
+            {#each columns as c}
+                <th>
+                    {#if c.name !== undefined}
+                        <SortIcon name={c.name} displayName={c.displayName} bind:sortBy />
+                    {/if}
+                </th>
+            {/each}
+        </tr>
+    </thead>
+    <tbody>
+        <slot />
+    </tbody>
+</table>
